7f40f110895fbfdb6b910b73db0af1e6ed8d63c8
Alexey Rahlin <ralexay@gmail.com>
2016-10-22 00:31:29 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='AudioBooksDownloader',
+    version='0.0.1',
+    packages=['tests', 'AudioBooksDownloader', 'AudioBooksDownloader.models'],
+    url='https://github.com/ralexay/AudioBooksDownloader',
+    license='',
+    author='ralexay',
+    author_email='',
+    description='', requires=['bs4', 'requests']
+)
